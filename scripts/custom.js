70a4c0d1807052de7b5e17f7ac0a698e84d67644
Theresa <tvollmer89@gmail.com>
2016-04-14 13:16:25 -0500

    Initial commit

---
+++ scripts/custom.js
@@ -0,0 +1,5 @@
+$('#homeSlide').slick({
+  nextArrow: '<img src="AD-Fire/images/icons/arrows-right.svg>',
+  prevArrow: '<img src="images/icons/arrows-left.svg>',
+// add the rest of your options here
+});
