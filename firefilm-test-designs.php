13e125c348f110ccd295d92d37627ffa050ef764
Theresa <tvollmer89@gmail.com>
2016-06-09 11:24:07 -0500

    table updates

---
+++ firefilm-test-designs.php
@@ -0,0 +1,189 @@
+<?php include("header.html"); ?>
+<div id="mainImage">
+  <img src="images/header-1.jpg" class="bg-img2">
+
+  <section class="pink-box">
+    <h1>Products</h1>
+  </section>
+</div>
+<div id="content">
+  <div class="content-inner container">
+    <div class="col-md-12">
+      <div class="row">
+        <h2>A/D&reg; Firefilm III</h2>
+        <div class="table-responsive">
+          <table class="table">
+            <caption><h3>Fire Test Design Summary</h3></caption>
+            <thead>
+              <tr>
+                <th rowspan="2" scope="col">Construction Type</th>
+                <th colspan="5" scope="colgroup">Minimum W/D (M/D) for Indicated Ratings</th>
+                <th colspan="4" scope="colgroup">Design No.</th>
+              </tr>
+              <tr>
+                <td>3/4, h</td>
+                <td>1, h</td>
+                <td>1-1/2, h</td>
+                <td>2, h</td>
+                <td>3, h</td>
+                <td class="red">ULI</td>
+                <td class="red">ULC</td>
+                <td class="red">ITS</td>
+                <td class="red">FM</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th rowspan="5" scope="rowgroup">Columns - W Shapes</th>
+                <td>0.43 (24)</td>
+                <td>0.43 (24)</td>
+                <td>0.92 (53)</td>
+                <td>1.62 (94)</td>
+                <td>-</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X641.pdf">X641</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z610.pdf">Z610</a></td>
+                <td><a target="_blank" href="FF%20Designs/ITS%20Designs/ITS design AD-IMF 120-02.pdf">AD/IMF 120-02</a></td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>0.66 (38)</td>
+                <td>1.64 (95)</td>
+                <td>-</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X643.pdf">X643</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z612.pdf">Z612</a></td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>0.84 (49)</td>
+                <td>0.59 (34)</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X669.pdf">X669</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z626.pdf">Z626</a></td>
+                <td>-</td>
+                <td><a href="FF%20Designs/FM%20FF%20Designs/FM%20Column%20Method%206.pdf">Method 6</a></td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>1.28 (74)</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X670.pdf">X670</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z627.pdf">Z627</a></td>
+                <td>-</td>
+                <td><a href="FF%20Designs/FM%20FF%20Designs/Method%207%20corrected%206%2002%2006.pdf">Method 7</a></td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>2.26 (130)</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X639.pdf">X639</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z608.pdf">Z608</a></td>
+                <td><a target="_blank" href="FF%20Designs/ITS%20Designs/ITS design AD-IMF 180-01.pdf">AD/IMF 180-01<br />(Formerly AD/CA 180-01)</a>
+                </td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <th rowspan="4" scope="rowspan">Columns - HSS Shapes</th>
+                <td>A/P >= 0.22(44)</td>
+                <td>A/P >= 0.22(44)</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X673.pdf">X673</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z630.pdf">Z630</a></td>
+                <td>-</td>
+                <td><a href="FF%20Designs/FM%20FF%20Designs/FM%20Column%20Method%2010.pdf">Method 10</a></td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>A/P >= 0.30(59)</td>
+                <td>A/P >= 0.30(59)</td>
+                <td>-</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X671.pdf">X671</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z628.pdf">Z628</a></td>
+                <td>-</td>
+                <td><a href="FF%20Designs/FM%20FF%20Designs/FM%20Column%20Method%208.pdf">Method 8</a></td>
+              </tr>
+              <tr>
+                <td>A/P >= 0.18(36)</td>
+                <td>A/P >= 0.27(54)</td>
+                <td>A/P >= 0.30(60)</td>
+                <td>A/P >= 0.47(93)</td>
+                <td>-</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X642.pdf">X642</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z611.pdf">Z611</a></td>
+                <td><a target="_blank" href="FF%20Designs/ITS%20Designs/ITS design AD-IMF 90-01.pdf">AD/IMF 90-01<br />(Formerly AD/CA 90-02)</a></td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>A/P >= 0.46(36)</td>
+                <td>A/P >= 0.46(36)</td>
+                <td>A/P >= 0.46(36)</td>
+                <td>A/P >= 0.46(36)</td>
+                <td>A/P >= 0.46(36)</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_X672.pdf">X672</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_Z629.pdf">Z629</a></td>
+                <td>-</td>
+                <td><a href="FF%20Designs/FM%20FF%20Designs/FM%20Column%20Method%209.pdf">Method 9</a></td>
+              </tr>
+              <tr>
+              <th rowspan="3" scope="rowspan">Beams / Floors</th>
+                <td>B - 0.51(30)</td>
+                <td>B - 0.51(30)</td>
+                <td>R - 0.51(30)</td>
+                <td>R - 0.51(30)</td>
+                <td>-</td>
+                <td>-</td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_F906.pdf">F906</a></td>
+                <td><a target="_blank" href="FF%20Designs/ITS%20Designs/ITS design AD-IMF 120-01.pdf">AD/IMF 120-01<br />(Formerly AD/CA 120-01)</a>
+                </td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>B - 0.82(48)</td>
+                <td>R - 0.82(48)</td>
+                <td>R - 0.82(48)</td>
+                <td>R - 0.82(48)</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_D948.pdf">D948</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_F910.pdf">F910</a></td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>U - 0.80(46)</td>
+                <td>R - 0.80(46)<br />B - 0.84(48)</td>
+                <td>-</td>
+                <td><a target="_blank" href="FF%20Designs/UL%20Designs/UL_design_D941.pdf">D941</a></td>
+                <td><a target="_blank" href="FF%20Designs/ULC%20Designs/ULC_design_F912.pdf">F912</a></td>
+                <td>-</td>
+                <td><a href="FF%20Designs/FM%20FF%20Designs/FM Floor Ceiling Design No 31.pdf">No. 31</a></td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="10">R = Restrained Assembly, B = Restrained Assembly + Unrestrained Assembly and Beam, U = Unrestrained Assembly and Beam</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div class="disclaimer">
+          <p>This summary contains information we believe to be accurate and current as of <strong>April 8, 2015.</strong><br />Please <a href="contact.php">contact</a> your A/D representative for the latest design information.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include("footer.php"); ?>
---
+++ test-designs-2.php
@@ -0,0 +1,195 @@
+<?php include("header.html"); ?>
+<div id="mainImage">
+  <img src="images/header-1.jpg" class="bg-img2">
+
+  <section class="pink-box">
+    <h1>Products</h1>
+  </section>
+</div>
+<div id="content">
+  <div class="content-inner container">
+    <div class="col-md-12">
+      <div class="row">
+        <h2>Southwest Fireproofing Products</h2>
+        <div id="sw-table" class="table-responsive">
+          <table border="0" class="table">
+            <caption>
+              <h3>Popular UL Design Table</h3>
+            </caption>
+            <thead>
+              <tr>
+                <th rowspan="2"></th>
+                <th rowspan="2" scope="colgroup">Design</th>
+                <th colspan="5" scope="colgroup">Ratings</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>1.5</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th rowspan="2" scope="rowgroup">Protected Floor Designs</th>
+                <td>D-739</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <td>D-788</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <th scope="rowgroup">Unprotected Floor Designs</th>
+                <td>D-949</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <th rowspan="3" scope="rowgroup">Concrete Floor Designs</th>
+                <td>J-704</td>
+                <td>-</td>
+                <td>-</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <td>J-709</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>J-718</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <th rowspan="2" scope="rowgroup">Protected Roof Designs</th>
+                <td>P-701</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>P-741</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <th rowspan="2" scope="rowgroup">Unprotected Roof Designs</th>
+                <td>P-921</td>
+                <td>R</td>
+                <td>R</td>
+                <td>R</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>P-937</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <th rowspan="2" scope="rowgroup">Wide Flange Columns</th>
+                <td>X-772</td>
+                <td>C</td>
+                <td>C</td>
+                <td>C</td>
+                <td>C</td>
+                <td>C</td>
+              </tr>
+              <tr>
+                <td>Y-725</td>
+                <td>C</td>
+                <td>C</td>
+                <td>C</td>
+                <td>C</td>
+                <td>C</td>
+              </tr>
+              <tr>
+                <th scope="rowgroup">Pipe and Tube Columns</th>
+                <td>X-771</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+              </tr>
+              <tr>
+                <th rowspan="2" scope="rowgroup">Roof Beams/Joist Columns</th>
+                <td>S-739</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>B</td>
+                <td>-</td>
+              </tr>
+              <tr>
+                <td>S-740</td>
+                <td>U</td>
+                <td>U</td>
+                <td>U</td>
+                <td>U</td>
+                <td>-</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="7">
+                  R = Restrained Assembly or Beam, B = Restrained Assembly &amp; Unrestrained Assembly, C = Column (R &amp; U not applicable)
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div class="table-responsive">
+          <table id="key" class="table">
+            <tbody>
+              <tr>
+                <td>5 GP gypsum 15 pcf</td>
+                <td>5 MD/gypsum 22 pcf</td>
+                <td>7 GP/Portland 22 pcf</td>
+                <td>7 HD/Portland 40 pcf</td>
+              </tr>
+              <tr>
+                <td colspan="4"> 5 GP, 5 MD, 7 GP and 7 HD listed in all designs.</td>
+              </tr>
+              <tr>
+                <td colspan="4"> For exterior exposure applications, use Carboline' s Pyrocrete products.</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include("footer.php"); ?>
